99dfff64f15ef16c6748fc3d29dc6baea4c0bbe7
Karel Vaculik <vacullik@gmail.com>
2015-10-28 11:56:43 +0100

    Update main method

--- src/main/java/cz/muni/fi/EsperStockDemo.java
+++ src/main/java/cz/muni/fi/EsperStockDemo.java
@@ -14,8 +14,12 @@ public class EsperStockDemo
 {
     public static void main( String[] args )
     {
-        int numOfStocks = 50;
+        int numOfStocks = 100;
         int changeIndex = 10;
+
+        if (args.length == 1) {
+            String arg =
+        }
 
         if (args.length == 2) {
             int arg1, arg2;
@@ -24,7 +28,6 @@ public class EsperStockDemo
                 arg1 = Integer.parseInt(args[0]);
                 arg2 = Integer.parseInt(args[1]);
             } catch(NumberFormatException ex) {
-                System.out.println(ex);
                 printHelp();
                 return;
             }
@@ -41,26 +44,12 @@ public class EsperStockDemo
 
         List<Object> stream1 = EventStreamGenerator.generateStockStream(numOfStocks, changeIndex);
         List<Object> stream2 = EventStreamGenerator.generateStockStream(numOfStocks, changeIndex);
-//        List<Object> stream3 = EventStreamGenerator.generateStockStream(numOfStocks, changeIndex);
-//        List<Object> stream4 = EventStreamGenerator.generateStockStream(numOfStocks, changeIndex);
 
         StockMonitor monitor = new StockMonitor();
         monitor.addStream(stream1);
         monitor.addStream(stream2);
-//        monitor.addStream(stream3);
-//        monitor.addStream(stream4);
         monitor.start();
-        System.out.println("FINAL RESULT: " + monitor.getStockAlertResults().getNumOfResults());
-
-//        for (Object stock : stream1) {
-//            Stock s = (Stock) stock;
-//            System.out.println(s.toString());
-//        }
-//        for (Object stock : stream2) {
-//            Stock s = (Stock) stock;
-//            System.out.println(s.toString());
-//        }
-
+        monitor.closeServiceProvider();
     }
 
     private static void printHelp() {
